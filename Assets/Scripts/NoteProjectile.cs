21be2f3c293a2f650dc29a0274fa62a28c0c97d2
James Antiporda <james.antiporda@obf.ateneo.edu>
2022-09-08 18:40:48 +0800

    Initial commit

---
+++ Assets/Scripts/NoteProjectile.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteProjectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float initialYPosition;
+    public float finalYPosition;
+    public float velocity;
+    private float xPosition;
+    private Vector3 initialPosition;
+    private Vector3 finalPosition;
+
+    void Start()
+    {
+        initialPosition = new Vector3(transform.position.x, initialYPosition, 0);
+        finalPosition = new Vector3(transform.position.x, finalYPosition, 0);
+        transform.position = initialPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(0, velocity * Time.deltaTime, 0);
+        if (transform.position.y <= finalYPosition)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void InstantiateNote(int midiValue)
+    {
+        xPosition += (midiValue - 21) * (20.8f / 88);
+        transform.position = new Vector3(xPosition, transform.position.y, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    float moveSpeed = 10;
+
+    bool moveUp, moveDown, moveLeft, moveRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveUp = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
+        moveDown = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
+        moveLeft = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+        moveRight = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+
+        float moveAmount = moveSpeed * Time.fixedDeltaTime;
+        Vector2 move = Vector2.zero;
+
+        if (moveUp)
+        {
+            move.y += moveAmount;
+        }
+
+        if (moveDown)
+        {
+            move.y -= moveAmount;
+        }
+
+        if (moveLeft)
+        {
+            move.x -= moveAmount;
+        }
+
+        if (moveRight)
+        {
+            move.x += moveAmount;
+        }
+
+        float moveMagnitude = Mathf.Sqrt(move.x * move.x + move.y * move.y);
+
+        if (moveMagnitude > moveAmount)
+        {
+            float ratio = moveAmount / moveMagnitude;
+            move *= ratio;
+        }
+
+        pos += move;
+        /*if (pos.x <= -0.84f)
+        {
+            pos.x = -0.84f;
+        }
+        if (pos.x >= 18.7f)
+        {
+            pos.x = 18.7f;
+        }
+        if (pos.y <= 1.22f)
+        {
+            pos.y = 1.22f;
+        }
+        if (pos.y >= 8.75f)
+        {
+            pos.y = 8.75f;
+        }
+        */
+
+        transform.position = pos;
+    }
+}
