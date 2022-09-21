67180332a43e6845ceba4611cfc8a7c33a5413e4
James Antiporda <james.antiporda@obf.ateneo.edu>
2022-09-21 18:49:20 +0800

    Added multiline .csv reading

--- Assets/Scripts/CSVReader.cs
+++ Assets/Scripts/CSVReader.cs
@@ -6,27 +6,58 @@ public class CSVReader : MonoBehaviour
 {
     public TextAsset features;
     private string[] headers;
-    private string[] valuesString;
+    private string[][] valuesString;
+    private float[][] valuesRows;
+    private int numberOfValueRows;
+    private int numberOfColumns;
     private List<float> values = new List<float>();
 
     void Start()
     {
         string[] rows = features.text.Split(new char[] {'\n'});
-
+        numberOfValueRows = rows.Length - 2;
         headers = rows[0].Split(new char[] {','});
-        valuesString = rows[1].Split(new char[] {','});
+        numberOfColumns = headers.Length;
+        Debug.Log("no of columns " + headers.Length);
+        valuesRows = new float[numberOfValueRows][];
+        valuesString = new string[numberOfValueRows][];
+        Debug.Log("no of rows" + numberOfValueRows);
 
+        for(int i = 0; i < numberOfValueRows; i++)
+        {
+            valuesString[i] = rows[i + 1].Split(new char[] { ',' });
+        }
+
+        float[] dummyFloatArray;
+
+        for (int j = 0; j < numberOfValueRows; j++)
+        {
+            dummyFloatArray = new float[numberOfColumns];
+            for (int k = 0; k < numberOfColumns; k++)
+            {
+                dummyFloatArray[k] = float.Parse(valuesString[j][k]);
+            }
+            valuesRows[j] = dummyFloatArray;
+        }
+
+        /*
         foreach(string v in valuesString)
         {
             values.Add(float.Parse(v));
         }
+        */
 
-        Debug.Log(headers[0] + " = " + values[0]);
-        Debug.Log(GetValue("Repeated_Notes"));
+        Debug.Log(GetValue("Note_Density", 5));
     }
 
-    float GetValue(string data)
+    float GetValue(string data, int section)
     {
+        if (section > numberOfValueRows || section <= 0)
+        {
+            Debug.Log("Index Out of Range! There is no section with number: " + section);
+            return 0;
+        }
+
         int i = 0;
         foreach(string header in headers)
         {
@@ -36,7 +67,7 @@ public class CSVReader : MonoBehaviour
             }
             i++;
         }
-        return values[i];
+        return valuesRows[section - 1][i];
     }
 
     // Update is called once per frame
