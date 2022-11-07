6094a29e3c889ac1908c21484ee7b2ef78067555
veebeejay <jose.banal@obf.ateneo.edu>
2022-11-07 17:05:30 +0800

    Added NearMiss Script

---
+++ Assets/Scripts/NearMissScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NearMissScript : MonoBehaviour
+{
+    public bool isActive;
+    private SpriteRenderer spriteRenderer;
+    float nearMissTimer = 0;
+    float nearMissShowWindow = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isActive = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = isActive;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive == true)
+        {
+            nearMissTimer += Time.deltaTime;
+            if(nearMissTimer >= nearMissShowWindow)
+            {
+                HideNearMiss();
+            }
+        }
+    }
+
+    public void ShowNearMiss()
+    {
+        isActive = true;
+        spriteRenderer.enabled = isActive;
+    }
+
+    public void HideNearMiss()
+    {
+        nearMissTimer = 0.0f;
+        isActive = false;
+        spriteRenderer.enabled = isActive;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Attack" || collision.tag == "SmallDestroyable" || collision.tag == "AttackThrough")
+        {
+            ShowNearMiss();
+        }
+    }
+}
